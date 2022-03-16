2924cf8747f70438a62019c42ba0ca12431e0b72
Bryan Tung <bryantung89@gmail.com>
2022-03-16 13:57:29 +0800

    - imported ToneJS - piano tones for keys event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useCallback } from 'react';
 import './App.css';
+import * as Tone from "tone";
+
+const keyboardMapping = {
+  "a": "C4",
+  "w": "C#4",
+  "s": "D4",
+  "e": "D#4",
+  "d": "E4",
+  "f": "F4",
+  "t": "F#4",
+  "g": "G4",
+  "y": "G#4",
+  "h": "A4",
+  "u": "A#4",
+  "j": "B4",
+  "k": "C5",
+  "o": "C#5",
+  "l": "D5",
+  "p": "D#5",
+  ";": "E5",
+  ":": "F5",
+  "[": "F#5",
+  "'": "G5",
+}
 
 function App() {
+  const sampler = new Tone.Sampler({
+    urls: {
+      "C4": "C4.mp3",
+      "D#4": "Ds4.mp3",
+      "F#4": "Fs4.mp3",
+      "A4": "A4.mp3",
+    },
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
+  }).toDestination();
+  
+  const attackOnKey = useCallback(async (event) => {
+    if (!event.repeat) {
+      await Tone.start();
+      keyboardMapping[event.key] && sampler.triggerAttack(keyboardMapping[event.key]);
+    }
+  }, [sampler]);
+  
+  const releaseOnKey = useCallback((event) => {
+    keyboardMapping[event.key] && sampler.triggerRelease(keyboardMapping[event.key]);
+  }, [sampler]);
+
+  useEffect(()=> {
+    console.log("mounted")
+    document.addEventListener("keydown", attackOnKey);
+    document.addEventListener("keyup", releaseOnKey);
+    // on unmount
+    return () => {
+      console.log("unmounted")
+      document.removeEventListener("keydown");
+      document.removeEventListener("keyup");
+    }
+  }, [sampler, attackOnKey, releaseOnKey]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
