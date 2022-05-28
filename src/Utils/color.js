9cc2d689f5802715bcfa86d62095760c2c501fc5
Bryan Tung <bryantung89@gmail.com>
2022-05-28 17:57:02 +0800

    new utils for color and key mapping

---
+++ src/Utils/color.js
@@ -0,0 +1,3 @@
+export const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+export const altRainbow = ["#F75839", "#F3B72F", "#56A754", "#3160A3", "#A347BF"];
--- src/Utils/key.js
+++ src/Utils/key.js
@@ -1,9 +1,15 @@
-const noteKeysMap = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+export const fullKeysMap = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+export const whiteKeysMap = ["C", "D", "E", "F", "G", "A", "B"];
+
+export const blackKeysMap = ["C#", "D#", "F#", "G#", "A#"];
+
+export const blackKeysWithFillerMap = ["X0", "C#", "D#", "X1", "F#", "G#", "A#", "X2"];
 
 export function getKeyFromNumericNote(note) {
   const octave = Math.floor(note / 12) - 1;
   if (octave >= 0 && octave < 8) {
-    return `${noteKeysMap[note % 12]}${octave}`;
+    return `${fullKeysMap[note % 12]}${octave}`;
   }
   return undefined;
 }
