7037cdd43b4dc2dead07c8b72aac7645f4ef3e84
Bryan Tung <bryantung89@gmail.com>
2022-07-31 18:05:00 +0800

    added firebase connection and log in-out user and session

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { child, getDatabase, onDisconnect, push, ref, set } from "firebase/database";
+import PropTypes from "prop-types";
+import { useEffect } from "react";
 import styled from 'styled-components';
 import Keyboard from "./Components/Keyboard";
 
@@ -9,12 +12,52 @@ const AppComponent = styled.div`
   justify-content: center;
 `;
 
-function App() {
+function App({
+  firebase,
+  sessionId
+}) {
+  // const [userRef, setUserRef] = useState(null);
+  // const [sessionRef, setSessionRef] = useState(null);
+  // const [db, setDb] = useState(null);
+
+  useEffect(() => {
+    const _db = getDatabase(firebase);
+    // setDb(_db);
+
+    const usersRef = ref(_db, "activeUsers");
+    const sessionsRef = ref(_db, "sessions");
+    /**
+     * Push a new user in activeUsers list
+     */
+    const _userRef = push(usersRef);
+    set(_userRef, {
+      id: _userRef.key,
+      timestamp: +new Date(),
+      playingKeys: []
+    });
+    // setUserRef(_userRef);
+
+    const _sessionRef = !!sessionId
+      ? child(sessionsRef, `${sessionId}`)
+      : push(sessionsRef);
+    set(child(_sessionRef, `users/${_userRef.key}`), true);
+    // setSessionRef(_sessionRef);
+
+    // cleanup when disconnects
+    onDisconnect(_userRef).remove();
+    onDisconnect(_sessionRef).remove(`users/${_userRef.key}`);
+  }, [firebase, sessionId]);
+
   return (
     <AppComponent>
       <Keyboard />
     </AppComponent>
   );
+}
+
+App.propTypes = {
+  firebase: PropTypes.object.isRequired,
+  sessionId: PropTypes.string
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiCIJB9zf-i_NP0ufIBrRVG0MYwmS4qK0",
+  authDomain: "teaching-piano.firebaseapp.com",
+  databaseURL: "https://teaching-piano-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "teaching-piano",
+  storageBucket: "teaching-piano.appspot.com",
+  messagingSenderId: "751654117803",
+  appId: "1:751654117803:web:99b1cf2b7aefff6d9ff2ee",
+  measurementId: "G-DEW1TWDWHW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App firebase={app} />
   </React.StrictMode>,
   document.getElementById('root')
 );
